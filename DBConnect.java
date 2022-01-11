1a5bb41b4d11e048552badea8f7953ec712219ec
yashwanthmarripalli <ec2-user@ip-172-31-43-173.ap-south-1.compute.internal>
2022-01-11 17:44:44 +0000

    Updated DBConnect.java

--- DBConnect.java
+++ DBConnect.java
@@ -14,3 +14,14 @@ Accept-Language: en-US,en;q=0.9
 Host: locallhost.com
 X-HTTPS: 1
 Content-Length: 0
+
+GET / HTTP/2.0
+Sec-Ch-Ua: " Not;A Brand";v="99", "Microsoft Edge";v="97", "Chromium";v="97"
+Sec-Ch-Ua-Mobile: ?0
+Sec-Ch-Ua-Platform: "Windows"
+Upgrade-Insecure-Requests: 1
+User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.71 Safari/537.36 Edg/97.0.1072.55
+Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9
+Sec-Fetch-Site: cross-site
+Sec-Fetch-Mode: navigate
+Sec-Fetch-Dest: document
